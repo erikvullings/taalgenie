7f48bed0d091eb4961fa574ef16e10b9f9da96f2
Erik Vullings <erik.vullings@gmail.com>
2019-01-04 16:14:47 +0100

    Import polyfill for custom-element.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'material-icons/iconfont/material-icons.css';
 import './styles.css';
 import m from 'mithril';
+import '@webcomponents/custom-elements';
 import { dashboardSvc } from './services/dashboard-service';
 
 m.route(document.body, dashboardSvc.defaultRoute, dashboardSvc.routingTable);
